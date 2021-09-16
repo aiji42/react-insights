73ce021cbd1ebfccd6f4280d6d2a6e7e8a8724d4
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-09-16 23:52:10 +0900

    add operation of data push server

--- lib/InsightsProvider.tsx
+++ lib/InsightsProvider.tsx
@@ -26,7 +26,10 @@ export const InsightsProvider: FC<InsightsProviderProps> = ({
 }) => {
   const ref = useRef<Array<InsightsRecord>>([])
   useEffect(() => {
-    const intervalRef = setInterval(() => console.log(ref.current), interval)
+    const intervalRef = setInterval(() => {
+      pushHarperData(ref.current)
+      ref.current = []
+    }, interval)
     return () => {
       clearInterval(intervalRef)
     }
@@ -36,4 +39,31 @@ export const InsightsProvider: FC<InsightsProviderProps> = ({
   }, [])
 
   return <Context.Provider value={{ push }}>{children}</Context.Provider>
+}
+
+const pushHarperData = (records: InsightsRecord[]) => {
+  if (records.length < 1) return
+  const headers = new Headers()
+  headers.append('Content-Type', 'application/json')
+  headers.append('Authorization', `${import.meta.env.VITE_HARPER_TOKEN}`)
+
+  const body = JSON.stringify({
+    operation: 'insert',
+    schema: 'react_insight',
+    table: 'insights',
+    records: records.map((record) => ({
+      ...record,
+      location: window.location,
+      userAgent: window.navigator.userAgent,
+      language: window.navigator.language
+    }))
+  })
+
+  const requestOptions = {
+    method: 'POST',
+    headers,
+    body
+  }
+
+  fetch('https://samle-cloud-aiji.harperdbcloud.com', requestOptions)
 }
